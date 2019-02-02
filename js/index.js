34fa7c2041bb5d2e3cadaba4d4aca81a3e44de34
Yurii <diesel6@i.ua>
2019-02-02 11:11:19 +0000

    fixed errors

--- js/index.js
+++ js/index.js
@@ -1,46 +1,6 @@
-//let greetings;                   let  const
-//alert(greetings);
-
-// string
-// boolean
-// number
-// null
-// undefined
-// Object
-// Symbol
-
-//let name = prompt'Enter your name';
-//let msg = 'Hello' + name;
-//alert(msg);
-
-//const age = parseInt(prompt('Enter your age'));
-// Зробити змінну, яка буде містити вік + 1
-// Вивести результат на екран
-//const newAge = age + 1;
-//alert('You are ' + checkAge(age));
-
-//function add(a, b, c){
-//    const res = a + b + c;
-//    return res;
-//}
-
-//const total = add(2, 2, 3);
-//alert(total);
-
-//function checkAge(age) {
-//    if (age <= 18) {
-//        return 'child';
-//    } else {
-//        return 'adult';
-//    }
-//}
-
-
 const a = parseFloat(prompt('Enter a'));
 const b = parseFloat(prompt('Enter b'));
 const c = parseFloat(prompt('Enter c'));
-let x1;
-let x2;
 
 const result = solveQuadrEq(a, b, c);
 alert(result);
@@ -48,8 +8,8 @@ alert(result);
 function solveQuadrEq(a, b, c){
     const discr = calcDisc(a, b, c);
     if (discr > 0) {
-        x1 = - b + Math.sqrt(discr) / ( 2 * a );
-        x2 = - b - Math.sqrt(discr) / ( 2 * a );
+        x1 = (-b + Math.sqrt(discr)) / ( 2 * a );
+        x2 = (-b - Math.sqrt(discr)) / ( 2 * a );
     }else {
         if (discr === 0){
             x1 = - ( b / (2 * a));
